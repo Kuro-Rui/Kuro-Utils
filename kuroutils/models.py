033ea530814d2233de5d3ab44b3a4b789017cb49
Louis Dominic <louisdominic80@gmail.com>
2023-09-27 00:35:29 +0700

    Perhaps PascalCase?

--- kuroutils/models.py
+++ kuroutils/models.py
@@ -1,6 +1,6 @@
-import collections
+from collections import defaultdict
 
 # For better format when debugging
-class defaultdict(collections.defaultdict):
+class DefaultDict(defaultdict):
     def __str__(self):
         return str({key: item for key, item in self.items()})
