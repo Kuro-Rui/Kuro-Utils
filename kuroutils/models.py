511ce8f23f7782a1221d50a0cbcd69cd7f750d03
Louis Dominic <louisdominic80@gmail.com>
2024-04-26 13:28:43 +0700

    Nevermind

--- kuroutils/models.py
+++ kuroutils/models.py
@@ -2,8 +2,5 @@ from collections import defaultdict
 
 # For better format when debugging
 class DefaultDict(defaultdict):
-    def __repr__(self):
-        return {key: value for key, value in self.items()}
-
     def __str__(self):
-        return str(self.__repr__())
+        return str({key: value for key, value in self.items()})
