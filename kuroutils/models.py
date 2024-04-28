ebd4f157c9af31aa4fef286c1a199fa17a6fcdb9
Louis Dominic <louisdominic80@gmail.com>
2024-04-28 09:47:11 +0700

    This is a work of art

--- kuroutils/models.py
+++ kuroutils/models.py
@@ -1,6 +1,18 @@
-from collections import defaultdict
+from typing import Any, Optional
 
-# For better format when debugging
-class DefaultDict(defaultdict):
-    def __str__(self):
-        return str({key: value for key, value in self.items()})
+class DefaultDict(dict):
+    """A dict that returns a default value if a key is missing."""
+
+    def __init__(self, default: Optional[Any] = None):
+        super().__init__()
+        self.default = default
+
+    def __getitem__(self, key):
+        try:
+            return super().__getitem__(key)
+        except KeyError:
+            return self.__missing__(key)
+
+    def __missing__(self, key):
+        self[key] = self.default
+        return self.default
